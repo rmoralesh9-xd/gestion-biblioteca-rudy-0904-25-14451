3543229a5242dc095de87079d9698aa19c3b3307
umg <umg@HUE-LAB-1F4.labhue.local>
2026-04-25 11:07:41 -0600

    Agregue la clase Biblioteca para la parte de servicios

--- gestion-biblioteca-rudy_0904_25_14451/src/main/java/com/rudy_0904_25_14451/gestionbiblioteca/servicio/Biblioteca.java
+++ gestion-biblioteca-rudy_0904_25_14451/src/main/java/com/rudy_0904_25_14451/gestionbiblioteca/servicio/Biblioteca.java
@@ -1,13 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.rudy_0904_25_14451.gestionbiblioteca.servicio;
 
-/**
- *
- * @author umg
- */
+import com.rudy0904_25_14451.gestionbiblioteca.modelo.Libro;
+import java.util.ArrayList;
+import java.util.List;
+ 
 public class Biblioteca {
+  
+    private List<Libro> libros;
+
+
+    public Biblioteca() {
+        this.libros = new ArrayList<>();
+    }
+
     
+    public void agregar(Libro libro) {
+        libros.add(libro);
+    }
+
+    
+    public Libro buscar(String titulo) {
+        for (Libro libro : libros) {
+            if (libro.getTitulo().equalsIgnoreCase(titulo)) {
+                return libro;
+            }
+        }
+        return null;
+    }
+
+   
+    public List<Libro> obtener() {
+        return libros;
+    }
+
+    public void imprimirLibros() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public Libro buscarPorTitulo(String tituloBusqueda) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void agregarLibro(Libro libro) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 }
+
