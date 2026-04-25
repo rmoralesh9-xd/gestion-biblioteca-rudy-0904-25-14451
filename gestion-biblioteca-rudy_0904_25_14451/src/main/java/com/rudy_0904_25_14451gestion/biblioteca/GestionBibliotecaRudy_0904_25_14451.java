74ea1461d86f9572732ec4e8bb165fc920cbf95d
umg <umg@HUE-LAB-1F4.labhue.local>
2026-04-25 10:27:24 -0600

    "He creado el proyecto y su estructura de paquetes"

---
+++ gestion-biblioteca-rudy_0904_25_14451/src/main/java/com/rudy_0904_25_14451gestion/biblioteca/GestionBibliotecaRudy_0904_25_14451.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.rudy_0904_25_14451gestion.biblioteca;
+
+/**
+ *
+ * @author umg
+ */
+public class GestionBibliotecaRudy_0904_25_14451 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
