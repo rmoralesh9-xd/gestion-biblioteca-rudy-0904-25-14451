d194b59d6bc6bf20d0e7401dc3708cefebd74d07
umg <umg@HUE-LAB-1F4.labhue.local>
2026-04-25 11:09:04 -0600

    "He creado el proyecto y su estructura de paquete

--- gestion-biblioteca-rudy_0904_25_14451/src/main/java/com/rudy_0904_25_14451/biblioteca/app/Main.java
+++ gestion-biblioteca-rudy_0904_25_14451/src/main/java/com/rudy_0904_25_14451/biblioteca/app/Main.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.rudy_0904_25_14451.biblioteca.app;
 
-/**
- *
- * @author umg
- */
+import com.rudy0904_25_14451.gestionbiblioteca.modelo.Libro;
+import com.rudy_0904_25_14451.gestionbiblioteca.servicio.Biblioteca;
+
+
 public class Main {
     
+
+
+    public static void main(String[] args) {
+
+        
+        Biblioteca biblioteca = new Biblioteca();
+
+       
+        biblioteca.agregarLibro(new Libro("FNAF", "SCOT CAWTON"));
+        biblioteca.agregarLibro(new Libro("1984", "George Orwell"));
+        biblioteca.agregarLibro(new Libro("Cien años de soledad", "Gabriel García Márquez"));
+
+     
+        try {
+            biblioteca.agregarLibro(new Libro("", "Autor desconocido"));
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error al agregar libro inválido: " + e.getMessage());
+        }
+
+       
+        System.out.println("\nLista de libros:");
+        biblioteca.imprimirLibros();
+
+      
+        String tituloBusqueda = "1984";
+        Libro encontrado = biblioteca.buscarPorTitulo(tituloBusqueda);
+
+      
+        if (encontrado != null) {
+            System.out.println("\nLibro encontrado: " + encontrado);
+        } else {
+            System.out.println("\nLibro no encontrado");
+        }
+    }
 }
