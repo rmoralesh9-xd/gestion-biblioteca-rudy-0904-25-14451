a409f9f5200e8e212420cb3fd79072730adbf13a
umg <umg@HUE-LAB-1F4.labhue.local>
2026-04-25 11:04:12 -0600

    Agregue la la clase Libro como parte del modelo

--- gestion-biblioteca-rudy_0904_25_14451/src/main/java/com/rudy0904_25_14451/gestionbiblioteca/modelo/Libro.java
+++ gestion-biblioteca-rudy_0904_25_14451/src/main/java/com/rudy0904_25_14451/gestionbiblioteca/modelo/Libro.java
@@ -1,13 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.rudy0904_25_14451.gestionbiblioteca.modelo;
 
-/**
- *
- * @author umg
- */
+
 public class Libro {
-    
+   
+
+   
+    private String titulo;
+    private String autor;
+    private int yearPublicacion;
+
+  
+    public Libro(String titulo, String autor, int yearPublicacion) {
+
+        if (titulo == null || titulo.trim().isEmpty()) {
+            throw new IllegalArgumentException("El título no puede ser null ni vacío");
+        }
+
+        if (autor == null || autor.trim().isEmpty()) {
+            throw new IllegalArgumentException("El autor no puede ser null ni vacío");
+        }
+
+        if (yearPublicacion <= 0) {
+            throw new IllegalArgumentException("El año de publicación debe ser mayor que 0");
+        }
+
+        this.titulo = titulo;
+        this.autor = autor;
+        this.yearPublicacion = yearPublicacion;
+    }
+
+    public Libro(String string, String autor_desconocido) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+   
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getYearPublicacion() {
+        return yearPublicacion;
+    }
+
+  
+    public boolean esAntiguo() {
+        return yearPublicacion < 2000;
+    }
 }
