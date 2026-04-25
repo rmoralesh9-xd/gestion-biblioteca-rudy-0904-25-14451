c1baa15e2e4598555db56a1fe116273bec72b20c
umg <umg@HUE-LAB-1F4.labhue.local>
2026-04-25 10:37:41 -0600

    Agregue la clase main para probar la aplicacion

---
+++ gestion-biblioteca-rudy_0904_25_14451/src/main/java/com/rudy_0904_25_14451/biblioteca/app/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.rudy_0904_25_14451.biblioteca.app;
+
+/**
+ *
+ * @author umg
+ */
+public class Main {
+    
+}
